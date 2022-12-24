b6e42d50272e0c302998a0a846664adfa8c0d654
Rakesh510 <111903813+Rakesh510@users.noreply.github.com>
2022-12-24 13:05:04 +0530

    Add files via upload

---
+++ Fudoo/src/test/java/com/medicareapp/FudooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicareapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
